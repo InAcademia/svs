576db01f7f2fa336e1770843d99694e2fcf41a4e
Niels van Dijk <niels.vandijk@surfnet.nl>
2020-05-27 16:49:07 +0200

    Added from and to flields to logs to better monitor request/response handling Updated flow log to include from and to infromation Remove unused log status

--- src/svs/util/transaction_flow_logging.py
+++ src/svs/util/transaction_flow_logging.py
@@ -2,13 +2,16 @@ from datetime import datetime
 import logging
 logger = logging.getLogger('transaction_flow')
 
-def transaction_log(id, order, component, type, location, status, description = ''):
+def transaction_log(id, order, component, type, location, status, to = '', frm = '', description = '', root_cause = ''):
     log_entry = dict([('id', id),
                       ('component', component),
                       ('type', type),
                       ('location', location),
                       ('status', status),
+                      ('frm', frm or ''),
+                      ('to', to or ''),
                       ('description', description or ''),
+                      ('root_cause', root_cause or ''),
                       ('timestamp', datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]),
                 ])
     logger.info('%s::%s::%s' % (id, order, log_entry))
