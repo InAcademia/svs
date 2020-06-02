55090569b4346b62bcd0d13700e930bcf0746d4f
Niels van Dijk <niels.vandijk@surfnet.nl>
2020-06-02 13:42:28 +0200

    Updated package data

--- setup.py
+++ setup.py
@@ -23,8 +23,8 @@ setup(
             'data/i18n/locale/*/LC_MESSAGES/*.mo',
             'templates/*.mako',
             'site/static/*',
-        ],
-        'util': [
+            'site/static/css/*.css',
+            'site/static/js/*.js',
             'util/*',
         ],
     },
